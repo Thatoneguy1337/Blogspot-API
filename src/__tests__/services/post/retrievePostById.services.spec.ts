2668bd250faaae5934a63c8d6571bd033f0478ef
Victor Guterres <Vgb1337@gmail.com>
2024-02-09 11:03:05 -0300

    fix: fixed retrievePostById test

--- src/__tests__/services/post/retrievePostById.services.spec.ts
+++ src/__tests__/services/post/retrievePostById.services.spec.ts
@@ -3,80 +3,68 @@ import { generateSscNumber } from "../../mocks/users";
 import tokenMock from "../../integration/token.mock";
 import supertest from "supertest";
 import app from "../../../app";
+import { TPostRequest,TPostResponse } from "../../../interfaces/post.interfaces";
 
 describe('Post functions', () => {
   let userId: number;
-  let isAdmin: boolean;
-  let userEmail: string;
   let postId: number;
   
   const baseUrl: string = '/post';
 
   const prisma = new PrismaClient();
 
-    beforeAll( async () => {
-      const createdUser = await prisma.users.create({
-        data: {
-        fullname:"John Doe" , 
-        username:"johnthedoughy89" , 
-        email:`test-${Date.now()}@example.com`, 
-        password:"12345678", 
-        reset_password:"", 
-        user_img:"", 
-        bg_img:"",
-        is_banned:false,
-        is_moderator:false,
-        ssc_number:generateSscNumber(), 
-        telephone:"1122604433",
-        birthdate:"06/04/1989",
-        description:"",
-        zip_code:"20068397",
-        state:"Texas",
-        city:"El Passo",
-        street:"Benson Stt",
-        number:"267"
-        },
-          });
-          
-          isAdmin = createdUser.is_moderator;
-          userId = createdUser.id;
-          userEmail = createdUser.email;
+  beforeAll(async () => {
+    const createdUser = await prisma.users.create({
+      data: {
+        fullname: "John Doe",
+        username: "johnthedoughy89",
+        email: `test-${Date.now()}@example.com`,
+        password: "12345678",
+        reset_password: "",
+        user_img: "",
+        bg_img: "",
+        is_banned: false,
+        is_moderator: false,
+        ssc_number: generateSscNumber(),
+        telephone: "1122604433",
+        birthdate: "06/04/1989",
+        description: "",
+        zip_code: "20068397",
+        state: "Texas",
+        city: "El Passo",
+        street: "Benson Stt",
+        number: "267"
+      },
+    });
 
-      const createPost = await prisma.posts.create({
-            data: {
-                description: "White Pony is my favorite deftones album, the second one is Around the Fur",
-                user_id: userId 
-            },
-              });
-            
-            postId = createPost.id         
+    userId = createdUser.id;
 
-      
-      const listPostById = await prisma.posts.findUnique({
-              where: {
-                id: postId,
-              }
-            });
+    const createPost = await prisma.posts.create({
+      data: {
+        description: "White Pony is my favorite Deftones album, the second one is Around the Fur",
+        post_img: "suaimagemaqui.jpg",
+        user_id: userId
+      },
+    });
 
-          console.log(listPostById)
+    postId = createPost.id;
+  });
 
-
-      
-      });
-
-    afterAll(async () => {
+  afterAll(async () => {
     await prisma.posts.delete({
       where: { id: postId },
-        });
-      });
+    });
 
-    test(' should create a post', async () => {
-      const response = await supertest(app)
-      .get(`/post/${postId}`) 
-      console.log("Id do Post:", postId);
-
-      expect(response.status).toBe(200);
-    }); 
-   
-
+    await prisma.users.delete({
+      where: { id: userId },
+    });
   });
+
+  test('should get a post by id', async () => {
+    const response = await supertest(app).get(`/post/${postId}`);
+    console.log("Id do Post:", postId);
+    console.log("Resposta da requisição:", response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.id).toBe(postId); 
+  });
+});
